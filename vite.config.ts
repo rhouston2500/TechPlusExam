058292bff0517843708fc719879bc1bcdba8861f
rhouston2500 <rhouston@ampedlouisville.org>
2025-10-23 11:57:51 -0400

    Configure base path for GitHub Pages
    Set base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/TechPlusExam/', // This line is required for deploying to GitHub Pages under a repo subpath
   optimizeDeps: {
     exclude: ['lucide-react'],
-    base: '/TechPlusExam/',
   },
 });
